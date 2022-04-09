ad95e1d7f97f2499a7a46e1e72200108ffab91d2
cosimopp <manuelli.matteo01@gmail.com>
2022-04-10 01:18:45 +0200

    Update ex3.py

--- 2020.02.21/ex3.py
+++ 2020.02.21/ex3.py
@@ -1,9 +1,24 @@
+'''
+Scrivere un programma python o uno script bash che produca il listato del contenuto di una directory
+in ordine di suffisso.
+'''
 import os
 import sys
 
 #per ogni estensione
-dict 
+dictionary = {}
+#https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+arr = os.listdir('./')
 
-for walkingDirPath, subDirs, files in os.walk("."): #for name in files:, for name in subDirs: lista tutti i file/tutte le cartelle con loro path
-	for fileName in files:
-		fullPath = os.path.join(walkingDirPath, fileName) #path completa
+for element in arr:
+	fullPath = os.path.join("./", element)
+	if os.path.isdir(fullPath): continue
+	extension = os.path.splitext(fullPath)[1]
+	if extension in dictionary: #se è tra le chiavi
+		dictionary[extension].append(fullPath)
+	else:
+		dictionary[extension] = [fullPath]
+keylist = list(dictionary.keys())
+keylist = sorted(keylist, key=str.casefold) #altrimenti maiuscole hanno la precedenza
+for key in keylist:
+	print(f"{key}: {str(dictionary[key])}")
